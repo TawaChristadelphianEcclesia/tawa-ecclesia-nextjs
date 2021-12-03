781e5b1697869f3d39f8de4c7661e423b7241be3
JoshGore <joshua.gore8@gmail.com>
2021-12-03 04:49:03 +0000

    add global styles decorator and scripts for next images

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
 import "../styles/globals.css";
+import * as NextImage from "next/image";
+import { GlobalStyles } from "twin.macro";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -8,3 +18,10 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [(Story) => (
+  <div>
+    <GlobalStyles />
+    <Story />
+  </div>
+)]
