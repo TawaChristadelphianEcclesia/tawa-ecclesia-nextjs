67ad2a677f26164df7d44ae962361a3e9d3c47b5
JoshGore <joshua.gore8@gmail.com>
2021-12-07 08:20:14 +0000

    add wrapper to seo to enable rendering

--- common/components/elements/Seo.tsx
+++ common/components/elements/Seo.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export interface IMeta {
     name: string;
@@ -12,47 +12,52 @@ export interface ISeoData {
     preventIndexing: boolean;
     structuredData: any;
     metaImage: IImageData;
-} 
+}
 
-const Seo: React.FC<ISeoData> = ({ metaTitle, metaDescription, metaTags, preventIndexing, structuredData, metaImage }) => {
-
-
-  return (
-    <Head>
-      <title>{metaTitle}</title>
-      <meta name="description" content={metaDescription} key="description" />
-      <meta
-        name="twitter:card"
-        content="summary_large_image"
-        key="twitter:card"
-      />
-      <meta property="og:url" content={'url'} key="og:url" />
-      <meta property="og:title" content={metaTitle} key="og:title" />
-      <meta
-        property="og:description"
-        content={metaDescription}
-        key="og:description"
-      />
-      <meta
-        property="og:image"
-        content={metaImage.url}
-        key="og:image"
-      />
-      <meta property="og:type" content="website" />
-      <link rel="canonical" href={'url'} />
-      {metaTags &&
-        metaTags.map((meta, index) => (
-          <meta name={meta.name} content={meta.content} key={index} />
-        ))}
-      {preventIndexing && (
-        <>
-          <meta name="robots" content="noindex"></meta>
-          <meta name="googlebot" content="noindex"></meta>
-        </>
-      )}
-      <script type="application/ld+json">{structuredData}</script>
-    </Head>
-  );
-};
+const Seo = ({
+    metaTitle,
+    metaDescription,
+    metaTags,
+    preventIndexing,
+    structuredData,
+    metaImage,
+}: ISeoData) => (
+    <>
+        <Head>
+            <title>{metaTitle}</title>
+            <meta
+                name="description"
+                content={metaDescription}
+                key="description"
+            />
+            <meta
+                name="twitter:card"
+                content="summary_large_image"
+                key="twitter:card"
+            />
+            <meta property="og:url" content={"url"} key="og:url" />
+            <meta property="og:title" content={metaTitle} key="og:title" />
+            <meta
+                property="og:description"
+                content={metaDescription}
+                key="og:description"
+            />
+            <meta property="og:image" content={metaImage.url} key="og:image" />
+            <meta property="og:type" content="website" />
+            <link rel="canonical" href={"url"} />
+            {metaTags &&
+                metaTags.map((meta, index) => (
+                    <meta name={meta.name} content={meta.content} key={index} />
+                ))}
+            {preventIndexing && (
+                <>
+                    <meta name="robots" content="noindex"></meta>
+                    <meta name="googlebot" content="noindex"></meta>
+                </>
+            )}
+            <script type="application/ld+json">{structuredData}</script>
+        </Head>
+    </>
+);
 
 export default Seo;
