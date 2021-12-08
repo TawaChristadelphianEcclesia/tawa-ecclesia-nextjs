5591a8a4e4166d39bf46c45554e19f3710b9d56f
JoshGore <joshua.gore8@gmail.com>
2021-12-08 03:35:25 +0000

    added Hero Header component

---
+++ common/components/elements/heroHeader.tsx
@@ -0,0 +1,63 @@
+/** @jsxImportSource @emotion/react */
+import React, { ReactNode, useState } from "react";
+import tw, { css, styled } from "twin.macro";
+import Image from "next/image";
+
+interface IHeroHeader {
+    title: string | ReactNode;
+    subtitle: string | ReactNode;
+    image: string;
+    imageBlurDataURL?: string;
+    alt?: string;
+    textColor?: string;
+    textPosition?: "top" | "bottom";
+}
+
+const HeroHeader: React.FC<IHeroHeader> = ({
+    title,
+    subtitle,
+    image,
+    imageBlurDataURL,
+    alt,
+    textColor,
+    textPosition,
+}) => {
+    return (
+        <div
+            style={{
+                position: "relative",
+                width: `100%`,
+                marginLeft: 0,
+                marginRight: 0,
+                // marginBottom: `1.45rem`,
+                height: "60vh",
+                overflow: "hidden",
+            }}
+        >
+            <Image
+                src={image}
+                alt={alt || ""}
+                layout="fill"
+                sizes="100%"
+                objectFit="cover"
+                placeholder="blur"
+                blurDataURL={imageBlurDataURL}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    margin: "20px",
+                    color: textColor,
+                    ...(!textPosition || textPosition == "bottom"
+                        ? { bottom: 0 }
+                        : { top: 0 }),
+                }}
+            >
+                <h1 tw="text-5xl sm:text-7xl font-bold font-serif">{title}</h1>
+                <p tw="sm:text-2xl">{subtitle}</p>
+            </div>
+        </div>
+    );
+};
+
+export default HeroHeader;
