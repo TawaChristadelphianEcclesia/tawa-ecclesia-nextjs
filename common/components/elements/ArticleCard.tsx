daf5a1ae5dbaca54a39edca094d77baa47bb9dcd
JoshGore <joshua.gore8@gmail.com>
2021-12-16 10:09:26 +0000

    style tweaks to article cards / list, add pagination to list

--- common/components/elements/ArticleCard.tsx
+++ common/components/elements/ArticleCard.tsx
@@ -23,12 +23,12 @@ const ArticleCard: React.FC<IArticleCard> = ({
     datePublished,
     readingTime,
 }) => (
-    <div tw="hover:shadow-2xl hover:scale-105 border border-white hover:border-gray-50 rounded-md transition duration-300 overflow-hidden bg-white">
+    <div tw="shadow-md hover:shadow-2xl hover:scale-105 rounded-md transition duration-300 overflow-hidden bg-white">
         <Link href={url} passHref>
             <a>
                 <div tw="flex cursor-pointer">
-                    <div tw="flex-1 ml-2">
-                        <h3 tw="text-gray-900 font-sans text-xl font-bold">
+                    <div tw="flex-1 p-1 pl-3 border border-gray-200 rounded-l-md">
+                        <h3 tw="text-gray-900 font-sans text-lg sm:text-xl font-bold">
                             {title}
                         </h3>
                         <p tw="text-gray-900 font-sans font-semibold text-xs">
@@ -38,13 +38,13 @@ const ArticleCard: React.FC<IArticleCard> = ({
                             {readingTime} Minute Read •{" "}
                             {Intl.DateTimeFormat("en-nz").format(datePublished)}
                         </p>
-                        <p tw="font-sans text-sm">
+                        <p tw="font-sans text-sm my-2">
                             {summary?.length < 120
                                 ? summary
                                 : summary.substring(0, 120) + "..."}
                         </p>
                     </div>
-                    <div tw="flex-initial w-48 justify-self-end pl-4 hidden sm:block">
+                    <div tw="flex-initial w-48 justify-self-end hidden sm:block">
                         <div tw="aspect-w-3 aspect-h-2">
                             <Image
                                 src={titleImage.url}
--- common/components/elements/ArticleList.tsx
+++ common/components/elements/ArticleList.tsx
@@ -2,17 +2,22 @@
 import React, { ReactNode, useState } from "react";
 import tw, { css, styled } from "twin.macro";
 import ArticleCard, { IArticleCard } from "./ArticleCard";
-import Card from "./Card";
+import ArticlePagination from "./ArticlePagination";
 
 interface IArticleList {
     articles: IArticleCard[];
 }
 
 const ArticleList: React.FC<IArticleList> = ({ articles }) => (
-    <div tw="space-y-2">
-        {articles.map((post, i) => (
-            <ArticleCard {...post} key={i} />
-        ))}
+    <div tw="mb-10">
+        <div tw="flex flex-col space-y-4 pb-5">
+            {articles.map((post, i) => (
+                <ArticleCard {...post} key={i} />
+            ))}
+        </div>
+        <div tw="pt-5">
+            <ArticlePagination />
+        </div>
     </div>
 );
 
