4a936ba790bff88146d804ff5b4e3618c13638fc
JoshGore <joshua.gore8@gmail.com>
2021-12-16 10:10:29 +0000

    update packages and next configuration to use new features

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    swcMinify: true,
+    experimental: {
+        // concurrentFeatures: true,
+        // serverComponents: true,
+    },
+    reactStrictMode: true,
+    webpack5: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
 
-    return config;
-  },
-  images: {
-    domains: ["images.prismic.io", "tawa-ecclesial-website.cdn.prismic.io"],
-  },
+        return config;
+    },
+    images: {
+        domains: ["images.prismic.io", "tawa-ecclesial-website.cdn.prismic.io"],
+    },
 };
