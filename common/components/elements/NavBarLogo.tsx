136c8f5b1a13c966865465aa89967af90a8494d6
JoshGore <joshua.gore8@gmail.com>
2021-12-16 10:08:27 +0000

    add lin k to navbar logo

--- common/components/elements/NavBarLogo.tsx
+++ common/components/elements/NavBarLogo.tsx
@@ -2,22 +2,28 @@
 import React, { useState } from "react";
 import tw, { css, styled } from "twin.macro";
 import Image from "next/image";
+import Link from "next/link";
+import ConditionalLinkWrapper from "./ConditionalLinkWrapper";
 
 interface INavBarLogo {
-    logo: IImageData
+    logo: IImageData;
+    href?: string;
 }
 
-const NavBarLogo: React.FC<INavBarLogo> = ({logo}) => {
-    return(          
-    <div tw="h-12 w-12 m-0 p-0 mx-2 relative">
-        <Image
-        src={logo.url}
-        alt={logo.alt}
-        sizes="100%"
-        layout="fill"
-        objectFit="contain"
-        />
-    </div>)
-}
+const NavBarLogo: React.FC<INavBarLogo> = ({ logo, href }) => {
+    return (
+        <ConditionalLinkWrapper href={href}>
+            <div tw="h-12 w-12 m-0 p-0 mx-2 relative">
+                <Image
+                    src={logo.url}
+                    alt={logo.alt}
+                    sizes="100%"
+                    layout="fill"
+                    objectFit="contain"
+                />
+            </div>
+        </ConditionalLinkWrapper>
+    );
+};
 
-export default NavBarLogo
+export default NavBarLogo;
