a4992a3c5778f4e8a7ebda7b15ec50ae13b5bb7c
JoshGore <joshua.gore8@gmail.com>
2021-12-16 10:10:12 +0000

    use new content components for home page

--- common/components/templates/HomePage.tsx
+++ common/components/templates/HomePage.tsx
@@ -4,7 +4,8 @@ import DefaultLayout, { IGlobalData } from "../layouts/DefaultLayout";
 import { ISeoData } from "../elements/Seo";
 import EventList, { IEventList } from "../elements/EventList";
 import Card from "../elements/Card";
-import GradientContentContainer from "../elements/GradientContentContainer";
+import Gradient from "../elements/Gradient";
+import ContentSection from "../elements/ContentSection";
 import ContactForm from "../elements/ContactForm";
 import ImageCard from "../elements/ImageCard";
 
@@ -25,24 +26,29 @@ interface IHomePage {
 const HomePage: React.FC<IHomePage> = ({ globalData, pageData }) => (
     <DefaultLayout seo={pageData.seo} global={globalData}>
         <HeroHeader {...pageData.headerData} />
-        <GradientContentContainer>
-            <Card animate>Latest Article</Card>
-            <Card animate>
-                <EventList {...pageData.eventData} />
-            </Card>
-            <Card animate>
-                <ImageCard src="undraw_podcast.svg" alt="podcast illustration">
-                    <h2 tw="text-gray-900 font-bold text-2xl">Podcast</h2>
-                    <p>
-                        Interested in the truth of the bible? Join us as we
-                        discuss a range of biblical topics.
-                    </p>
-                </ImageCard>
-            </Card>
-            <Card animate>
-                <ContactForm />
-            </Card>
-        </GradientContentContainer>
+        <Gradient>
+            <ContentSection padTop padBottom>
+                <Card animate>Latest Article</Card>
+                <Card animate>
+                    <EventList {...pageData.eventData} />
+                </Card>
+                <Card animate>
+                    <ImageCard
+                        src="undraw_podcast.svg"
+                        alt="podcast illustration"
+                    >
+                        <h2 tw="text-gray-900 font-bold text-2xl">Podcast</h2>
+                        <p>
+                            Interested in the truth of the bible? Join us as we
+                            discuss a range of biblical topics.
+                        </p>
+                    </ImageCard>
+                </Card>
+                <Card animate>
+                    <ContactForm />
+                </Card>
+            </ContentSection>
+        </Gradient>
     </DefaultLayout>
 );
 
