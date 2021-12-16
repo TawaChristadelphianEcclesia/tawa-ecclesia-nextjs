10ccc1db72aaafd724d057d0ed660d6c9baf3384
JoshGore <joshua.gore8@gmail.com>
2021-12-16 10:07:01 +0000

    rename content container and seperate out gradient

---
+++ common/components/elements/Gradient.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import React, { ReactNode, useState } from "react";
+import tw, { css, styled } from "twin.macro";
+
+const Gradient: React.FC = ({ children }) => (
+    <div tw="bg-gradient-to-t from-gray-200 to-gray-50">{children}</div>
+);
+
+export default Gradient;
