4674758fa2fc18de5c143273b76b4f76666660b6
Joshua Gore <joshua_gore@eagle.co.nz>
2022-01-19 06:51:36 +0000

    wrap card in content section

--- common/components/modules/blocks/ImageCardBlock.tsx
+++ common/components/modules/blocks/ImageCardBlock.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import tw, { css, styled } from "twin.macro";
+import ContentSection from "../../elements/ContentSection";
 import ImageCard, { IImageCard } from "../../elements/ImageCard";
 // import HeroHeader, { IHeroHeader } from "../../elements/HeroHeader";
 
 export type IImageCardBlock = IImageCard;
 
 const ImageCardBlock: React.FC<IImageCardBlock> = (props) => (
-    <ImageCard {...props} />
+    <ContentSection>
+        <ImageCard {...props} />
+    </ContentSection>
 );
 
 export default ImageCardBlock;
