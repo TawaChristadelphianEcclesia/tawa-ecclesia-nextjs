40a8e9ef77f7eb3ce195ac8626cdfdf1d1c2a108
Joshua Gore <joshua_gore@eagle.co.nz>
2022-02-04 10:37:22 +0000

    fix image function to support svg

--- common/utils/ImageData.ts
+++ common/utils/ImageData.ts
@@ -5,11 +5,17 @@ type ImageSizes = "thumbnail" | "small" | "medium" | "large";
 export const getImageData = (
     attributes?: HomePageData_home_data_attributes_Blocks_ComponentBlocksVideoHighlight_replaceImage_data_attributes,
     imageSize: ImageSizes = "large"
-): IImageData =>
-    attributes
+): IImageData => {
+    const hasRequestedSize = !!attributes?.formats?.[imageSize];
+    const imageUrl = hasRequestedSize
+        ? attributes?.formats?.[imageSize]?.url
+        : attributes?.url;
+    const data = attributes
         ? {
               alt: attributes?.alternativeText || "",
-              url: attributes?.formats?.[imageSize]?.url || "",
+              url: imageUrl,
               blurDataUrl: attributes?.formats?.thumbnail?.url || "",
           }
         : { alt: "", url: "", blurDataUrl: "" };
+    return data;
+};
